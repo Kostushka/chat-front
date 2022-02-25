b4fc34ba4e9f3cb3645740c0eee5106353e31a27
otshellnick <exte92@gmail.com>
2022-02-25 18:59:32 +0300

    up login

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 export const LoginForm = () => {
-    return <div>Login</div>
+    return <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => console.log(values) //action to send server
+             }>
+        {() => {
+            return <Form>
+                <Field type="text" name="email" />
+                <Field type="password" name="password" />
+                <button type="submit">
+                    Submit
+                </button>
+            </Form>
+        }}
+    </Formik>
 }
