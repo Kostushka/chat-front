0fe3afc0772b4120a38baf3c7011515cce22c06b
otshellnick <exte92@gmail.com>
2022-03-02 21:09:51 +0300

    up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[hash].js',
-        publicPath: '/',
+        publicPath: './',
     },
     devServer: {
         port: 3000,
