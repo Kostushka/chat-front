0c552b655af955c0a3a1aaaf57caf9dcc08dce91
Kostushka <nastyatery47@yandex.ru>
2022-03-13 19:20:17 +0300

    Added chat field

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UiButton from '@components/UI/UiButton';
 import styles from './Chat.module.css';
 
 const Chat = () => {
+    const [message, setMessage] = useState([{}]);
+    const [value, setValue] = useState('');
+    const handleSubmit = () => {
+        const newMessage = {
+            value,
+        };
+        setMessage([...message, newMessage]);
+        setValue('');
+    };
     return (
         <div className={styles.container}>
             <div className={styles.info}>Info</div>
-            <div className={styles.chat}>Chat</div>
+            <div className={styles.chat}>
+                Chat
+                <div className={styles.field}>
+                    <div className={styles.field__container}>
+                        {message.map((el: any, i) => {
+                            if (el.value) {
+                                return (
+                                    <p className={styles.message} key={i}>
+                                        {el.value}
+                                    </p>
+                                );
+                            }
+                        })}
+                    </div>
+                </div>
+                <form>
+                    <textarea
+                        className={styles.textarea}
+                        value={value}
+                        onChange={(e) => setValue(e.currentTarget.value)}
+                    ></textarea>
+                    <div>
+                        <UiButton onClick={handleSubmit} type='button'>
+                            Отправить
+                        </UiButton>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
