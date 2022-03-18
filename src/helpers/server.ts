df589b558ca5dc11c7ff5aba9d03d4edcf69286b
Aleksandr <exte92@gmail.com>
2022-03-18 21:30:50 +0300

    up auth

--- src/helpers/server.ts
+++ src/helpers/server.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+import * as CookieHelper from "./cookie";
 
 const server = 'https://mmochat.online/';
+const Authorization = CookieHelper.get('chat_session_id');
 
 export const Server = (method: 'get'|'post'|'put'|'delete', url: string, data?: object) => {
-    return axios[method](`${server}${url}`, data)
+    return axios[method](`${server}${url}`, {...data, Authorization})
         .then(resp => {
             if(resp.data.status) return {...resp.data}
             return resp.data;
