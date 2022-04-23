21c9871f15557d907b68e1189ebd303b5af70812
Aleksandr <exte92@gmail.com>
2022-04-23 13:43:39 +0300

    up socket

--- src/containers/Chat/ChatForm/ChatForm.tsx
+++ src/containers/Chat/ChatForm/ChatForm.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { useTypedSelector } from '../../../store';
-import { AddMessagesActionCreator } from '../../../store/reducers/messagesReducer';
-import { getDataRooms } from '../../../api/getDataRooms';
+import React, {FC, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {useParams} from 'react-router-dom';
+import {useTypedSelector} from '../../../store';
+import {AddMessagesActionCreator} from '../../../store/reducers/messagesReducer';
+import {getDataRooms} from '../../../api/getDataRooms';
+import {socket} from "../../../helpers/socket";
 
 import styles from './ChatForm.module.css';
 import Info from './Info';
@@ -11,18 +12,24 @@ import Form from './Form';
 
 const ChatForm: FC = () => {
     const dispatch = useDispatch();
-    const { messages } = useTypedSelector((state) => state.messages);
-    const { rows } = useTypedSelector((state) => state.rooms);
-    const { roomId } = useParams();
+    const {messages} = useTypedSelector((state) => state.messages);
+    const {rows} = useTypedSelector((state) => state.rooms);
+    const {user} = useTypedSelector((state) => state.user);
+    const {roomId} = useParams();
+    const io = socket(Number(roomId)).connect();
 
     useEffect(() => {
         dispatch(getDataRooms());
+
+        return () => {
+            io.disconnect();
+        }
     }, []);
 
     return (
         <div className={styles.container}>
-            <Info rows={rows} roomId={roomId} />
-            <Form messages={messages} />
+            <Info rows={rows} roomId={roomId}/>
+            <Form messages={messages}/>
         </div>
     );
 };
