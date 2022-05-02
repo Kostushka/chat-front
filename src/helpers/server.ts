17ae2e3e3bd715af57321bd466bcf8d94511e95b
Kostushka <nastyatery47@yandex.ru>
2022-05-02 19:25:04 +0300

    Changed dev to prod

--- src/helpers/server.ts
+++ src/helpers/server.ts
@@ -1,22 +1,25 @@
-import axios from "axios";
-import * as CookieHelper from "./cookie";
-import {CONFIG} from "@config/config";
-const server: string = CONFIG.development.REST;
+import axios from 'axios';
+import * as CookieHelper from './cookie';
+import { CONFIG } from '@config/config';
+const server: string = CONFIG.production.REST;
 const Authorization = CookieHelper.get('chat_session_id') || '';
-axios.defaults.headers.common['Authorization'] = Authorization
+axios.defaults.headers.common['Authorization'] = Authorization;
 
-export const Server = (method: 'get'|'post'|'put'|'delete', url: string, data?: object) => {
-    return axios[method](`${server}${url}`, data)
-        .then(resp => {
-            const {data} = resp;
+export const Server = (
+    method: 'get' | 'post' | 'put' | 'delete',
+    url: string,
+    data?: object
+) => {
+    return axios[method](`${server}${url}`, data).then((resp) => {
+        const { data } = resp;
 
-            if(data.status === 401) {
-                CookieHelper.del('chat_session_id');
-                return location.replace('/login');
-            }
+        if (data.status === 401) {
+            CookieHelper.del('chat_session_id');
+            return location.replace('/login');
+        }
 
-            if(resp.data.status) return {...resp.data};
+        if (resp.data.status) return { ...resp.data };
 
-            return resp.data;
-        });
-}
+        return resp.data;
+    });
+};
--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -1,15 +1,16 @@
-import {io} from 'socket.io-client';
-import {CONFIG} from "@config/config";
-import * as CookieHelper from "./cookie";
+import { io } from 'socket.io-client';
+import { CONFIG } from '@config/config';
+import * as CookieHelper from './cookie';
 
 const token = CookieHelper.get('chat_session_id') || '';
 
-export const socket = (roomId: number) => io(CONFIG.development.SOCKET, {
-    auth: {
-        token
-    },
-    query: {
-        roomId
-    },
-    autoConnect: false
-});
+export const socket = (roomId: number) =>
+    io(CONFIG.production.SOCKET, {
+        auth: {
+            token,
+        },
+        query: {
+            roomId,
+        },
+        autoConnect: false,
+    });
