66222d019e58d6f0badfc7cbce1f5502d0cc0662
Kostushka <nastyatery47@yandex.ru>
2022-05-29 21:56:30 +0300

    Up socket

--- src/containers/Chat/ChatForm/ChatForm.tsx
+++ src/containers/Chat/ChatForm/ChatForm.tsx
@@ -1,35 +1,35 @@
-import React, {FC, useEffect} from 'react';
-import {useDispatch} from 'react-redux';
-import {useParams} from 'react-router-dom';
-import {useTypedSelector} from '../../../store';
-import {AddMessagesActionCreator} from '../../../store/reducers/messagesReducer';
-import {getDataRooms} from '../../../api/getDataRooms';
-import {socket} from "../../../helpers/socket";
-
-import styles from './ChatForm.module.css';
+import React, { FC, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useTypedSelector } from '../../../store';
+import { getDataRooms } from '../../../api/getDataRooms';
+import { socket } from '../../../helpers/socket';
+import { useChat } from '../../../hooks/useChat';
 import Info from './Info';
 import Form from './Form';
 
+import styles from './ChatForm.module.css';
+
 const ChatForm: FC = () => {
     const dispatch = useDispatch();
-    const {messages} = useTypedSelector((state) => state.messages);
-    const {rows} = useTypedSelector((state) => state.rooms);
-    const {user} = useTypedSelector((state) => state.user);
-    const {roomId} = useParams();
+    const { messages } = useTypedSelector((state) => state.messages);
+    const { rows } = useTypedSelector((state) => state.rooms);
+    const { user } = useTypedSelector((state) => state.user);
+    const { roomId } = useParams();
     const io = socket(Number(roomId)).connect();
+    useChat(Number(roomId));
 
     useEffect(() => {
         dispatch(getDataRooms());
-
         return () => {
             io.disconnect();
-        }
+        };
     }, []);
 
     return (
         <div className={styles.container}>
-            <Info rows={rows} roomId={roomId}/>
-            <Form messages={messages}/>
+            <Info rows={rows} roomId={roomId} />
+            <Form messages={messages} />
         </div>
     );
 };
--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -4,7 +4,7 @@ import * as CookieHelper from './cookie';
 
 const token = CookieHelper.get('chat_session_id') || '';
 
-export const socket = (roomId: number) =>
+export const socket = (roomId: number | null) =>
     io(CONFIG.production.SOCKET, {
         auth: {
             token,
