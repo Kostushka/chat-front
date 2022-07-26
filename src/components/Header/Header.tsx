dad7893ebf892ec8785db46a034056e437925261
Kostushka <nastyatery47@yandex.ru>
2022-07-26 20:22:20 +0300

    Added status

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,15 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useTypedSelector } from '../../store';
 import userPhoto from '../../assets/profile.svg';
+
 import styles from './Header.module.css';
 
 const Header: FC = () => {
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState('');
+    const changeEditMode = () => {
+        setEditMode(!editMode);
+    };
     const { user } = useTypedSelector((state) => state.user);
     return (
         <div className={styles.container}>
@@ -15,6 +21,7 @@ const Header: FC = () => {
                         <img src={userPhoto} alt='фото пользователя' />
                     )}
                 </li>
+
                 <div className={styles.user_info}>
                     <li>
                         <span>Имя пользователя:</span> {user.username}
@@ -28,8 +35,27 @@ const Header: FC = () => {
                         <span>Пол:</span> {user.gender}
                     </li>
                     <li>
-                        <span>Статус:</span> {user.role}
+                        <span>Активность:</span> {user.role}
                     </li>
+                    <div>
+                        {editMode ? (
+                            <span
+                                onClick={changeEditMode}
+                                className={styles.status}
+                            >
+                                {status || 'Статус отсутствует'}
+                            </span>
+                        ) : (
+                            <input
+                                type='text'
+                                placeholder='Введите свой статус'
+                                onChange={(e) => setStatus(e.target.value)}
+                                onBlur={changeEditMode}
+                                autoFocus={true}
+                                className={styles.input_status}
+                            />
+                        )}
+                    </div>
                 </div>
             </ul>
         </div>
